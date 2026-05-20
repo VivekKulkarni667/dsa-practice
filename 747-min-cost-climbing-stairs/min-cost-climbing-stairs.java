5f184de6abd51e8540da4dd6fc0ef88605a0b71e
Vivek <vivekvkbbb@gmail.com>
2026-05-21 02:26:45 +0530

    Time: 1 ms (37.94%) | Memory: 45 MB (23.09%) - LeetSync

---
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+class Solution {
+
+    public int mincost(int[] cost, int index, int[] dp) {
+
+        
+        if(index == 0 || index == 1) return cost[index];
+
+        // if already calculated then return from dp
+        if(dp[index] != -1) return dp[index];
+
+        // formula :
+        // mincost(i) = arr[i] + min(mincost(i-1), mincost(i-2))
+        return dp[index] = cost[index] + Math.min(mincost(cost,index - 1,dp), mincost(cost,index - 2,dp));
+    }
+
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+
+        // n is going from n-1 to 0
+        int[] dp = new int[n];
+
+        // filling dp array with -1
+        Arrays.fill(dp,-1);
+
+        // top can be reached from last stair or second last stair
+        return Math.min(mincost(cost,n - 1,dp), mincost(cost,n - 2,dp));
+    }
+}
