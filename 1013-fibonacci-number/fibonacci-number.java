f719910c0af6b36534ecfb8e3c376c30889f9f2a
Vivek <vivekvkbbb@gmail.com>
2026-05-21 00:58:44 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (5.02%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,17 +1,42 @@
+// memoization recursive dp
+
+// class Solution {
+
+//     public int fibo(int n,int[] dp) {
+//         if (n <=1) return n;
+//         // agar array mein already answer hai to uss ans ko lautado ! 
+//         if (dp[n] !=0 ) return dp[n];
+
+//         //ans nhi hai to isme store kardo current jo calculate kiya usko
+//        return dp[n] =  fibo(n - 1,dp) + fibo(n - 2,dp); 
+       
+//     }
+// public int fib(int n){
+//      int[] dp;
+//     dp = new int[n+1];  // index 0 ninda n varegu
+//     return fibo(n,dp);
+// }
+//     }
+
+
+
+
+
+
+// tabulation dp
 class Solution {
-static int[] dp; // creating array for knwon solutions already calculated
-    public int fibo(int n) {
-        if (n <=1) return n;
-        if (dp[n] !=0 ) return dp[n];
-        // if (n == 1) return 1;  two base case
-        int ans = fibo(n - 1) + fibo(n - 2);
-        dp[n] = ans;
-        return ans;
+    public int fib(int n) {
+        if (n <= 1) return n;
+
+        int[] dp = new int[n + 1];
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
     }
-public int fib(int n){
-    dp = new int[n+1];  // index o ninda n varegu
-    return fibo(n);
 }
-    }
-
-
