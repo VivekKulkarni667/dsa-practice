e5b4e1aef879c41fce0cbaa0e2da4e9480b2824b
Vivek <vivekvkbbb@gmail.com>
2026-05-20 23:02:24 +0530

    Time: 17 ms (9.16%) | Memory: 41.9 MB (68.45%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,11 +1,13 @@
 class Solution {
-static int[] dp;
+static int[] dp; // creating array for knwon solutions already calculated
     public int fibo(int n) {
         if (n <=1) return n;
+        if (dp[n] !=0 ) return dp[n];
         // if (n == 1) return 1;  two base case
         return fibo(n - 1) + fibo(n - 2);
     }
 public int fib(int n){
+    dp = new int[n+1];  // index o ninda n varegu
     return fibo(n);
 }
 
