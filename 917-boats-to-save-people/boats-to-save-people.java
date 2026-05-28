f8abea13799d55543f56640537080116d1c1925f
Vivek <vivekvkbbb@gmail.com>
2026-05-28 19:54:10 +0530

    Time: 20 ms (82.96%) | Memory: 56.7 MB (8.84%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.java
+++ 917-boats-to-save-people/boats-to-save-people.java
@@ -1,33 +1,27 @@
 class Solution {
-    public int numRescueBoats(int[] arr, int limit) {
+        public int numRescueBoats(int[] arr, int limit) {
 
+        Arrays.sort(arr);  
+        int n=arr.length;
+        int left =0 , right = n-1; // 0th index halka person , last index bhaari person
+        int count =0;
 
-Arrays.sort(arr);
-int n=arr.length;
-int left =0 , right = n-1; // 0th index halka person , last index bhaari person
-int count =0;
+        while(left<=right){  // because sorted array two pointer logic 
+        if(arr[left]+arr[right] <= limit){ // ek mota + ek patla <= boat limit
 
-while(left<=right){
-    if(arr[left]+arr[right] <= limit){
+                    left++;
+                            right--;
+                                }
+                                else right--;  // matlab sirf mota person hi jayega
+                                count++;
+                                }
+                                return count; // kitna boat chahiye saare person k liye return kardo 
 
-        left++;
-        right--;
-    }
-else right--;
-
-count++;
-
-}
-
-
-return count;
-
-
-    }
-}
+                                    }
+                                    }
 
 
 
 
-        
-          
+                                            
+                                                      
