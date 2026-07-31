87a1b16bcff7578f42fd6935389b64020d270b84
Vivek <vivekvkbbb@gmail.com>
2026-07-31 07:46:07 +0530

    Time: 2 ms (92.10%) | Memory: 47.6 MB (76.60%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,29 +1,54 @@
 class Solution {
     public void moveZeroes(int[] arr) {
-
+// my logic 
         int n = arr.length;
+        // for (int i = 0; i < n - 1; i++) {
+        //     if (arr[i] == 0) {
+        //         int j = i + 1;
+        //         // Find the next non-zero element
+        //         while (j < n && arr[j] == 0) {
+        //             j++;
+        //         }
+        //         // No non-zero element found
+        //         if (j == n) {
+        //             break;
+        //         }
+        //         int temp = arr[i];
+        //         arr[i] = arr[j];
+        //         arr[j] = temp;
+        //     }
+        // }
 
-        for (int i = 0; i < n - 1; i++) {
 
-            if (arr[i] == 0) {
 
-                int j = i + 1;
 
-                // Find the next non-zero element
-                while (j < n && arr[j] == 0) {
-                    j++;
-                }
+// optimized 
+ int k=0;
+ for(int i=0; i<n; i++){
+    if(arr[i] !=0){
 
-                // No non-zero element found
-                if (j == n) {
-                    break;
-                }
+        int temp = arr[i];
+                arr[i] = arr[k];
+                arr[k] = temp;
 
-                // Swap
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-        }
+
+k++;
+
+    }
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
