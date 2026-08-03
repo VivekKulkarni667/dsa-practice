d6d68f743357ec095c4346a6961587432ae120b2
Vivek <vivekvkbbb@gmail.com>
2026-08-03 14:38:51 +0530

    Time: 4 ms (41.52%) | Memory: 175.8 MB (8.44%) - LeetSync

---
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] rearrangeArray(int[] arr) {
+        int negative =1;
+        int positive=0;
+        int n=arr.length;
+        int[] ans = new int[n];
+
+        for(int i=0; i<n; i++){
+            if(arr[i]<0){ 
+            ans[negative]=arr[i];
+            negative+=2;
+            }
+
+           if(arr[i]>0)
+            {
+                ans[positive]=arr[i];
+                positive+=2;
+            }
+            }
+        
+
+     
+ 
+        return ans;
+
+
+    }
+}
